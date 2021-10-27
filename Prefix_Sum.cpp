4bda4d048f5812ddc00cd730adb9b667a759caf3
Abhishek Jha <76877870+abhishekjha009@users.noreply.github.com>
2021-10-27 09:50:18 +0530

    Add files via upload

---
+++ Prefix_Sum.cpp
@@ -0,0 +1,40 @@
+/* Given array a of N integers . Given q queries and in each query given two integers l and r.
+Print sum of integers from index l to r(l,r included)
+1<=N<=10^5
+1<=Q<=10^5
+1<=a[i]<=10^9
+1<=l,r<=N
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+10;
+int a[N];
+int pf[N];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        pf[i]=pf[i-1]+a[i];
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        cout<<pf[r]-pf[l-1]<<endl;
+    }
+    
+    return 0;
+}
